d0c002c36395cff577a9ecf0ff96631bf37c5ffb
J.J. Idt & Tanner Steward <student@epicodus.com>
2014-07-22 14:29:55 -0700

    add functionality to give multiple allergens for an allergy score

---
+++ README.md
@@ -0,0 +1,10 @@
+#Allergy Score
+
+JJ Idt & Tanner Steward
+
+Allows a user to input a score and it returns the list of allergies hey are allergic to.
+
+##Instructions
+
+Enter in a number and the function will return a list of allergens that you are allergic to.
+
--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -5,4 +5,8 @@ describe('allergies') do
   it("takes a number and returns the corresponding allergy") do
     allergies(1).should(eq('eggs'))
   end
+
+  it("takes the users input number and returns list of allergies that add up to that score") do
+    allergies(14).should(eq('strawberries, shellfish, peanuts'))
+  end
 end
